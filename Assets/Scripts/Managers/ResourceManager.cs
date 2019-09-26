de713c14e360e349e3648b91b66f39e94c5c85ec
Em3rgencyLT <em3rgency.lt@gmail.com>
2019-09-26 14:49:51 +0300

    feat: add basic resource data files

---
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Data;
+using UnityEngine;
+
+namespace Managers
+{
+    public class ResourceManager : MonoBehaviour
+    {
+        [SerializeField] private List<Resource> _resources;
+    }
+}
