9ad6841b5c5ac5d8394abaf2c83a5b31b39eebde
Em3rgencyLT <em3rgency.lt@gmail.com>
2019-10-02 15:05:48 +0300

    feat: add resource tracking, begin work on resource UI

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -7,10 +7,10 @@ namespace Managers
 {
     public class ResourceManager : MonoBehaviour
     {
-        [SerializeField] private List<Resource> _resources;
+        [SerializeField] private List<Resource> resources;
         [SerializeField] private Sprite defaultResourceSprite;
 
-        private void Start()
+        private void Awake()
         {
             AddDefaultResources();
         }
@@ -22,7 +22,37 @@ namespace Managers
             var lumber = new Resource(ResourceType.Lumber, defaultResourceSprite, 10);
             var slag = new Resource(ResourceType.Concrete, defaultResourceSprite, 50);
             
-            _resources.AddRange(new[]{jack, metal, lumber, slag});
+            resources.AddRange(new[]{jack, metal, lumber, slag});
+        }
+
+        public List<Resource> Resources => resources;
+
+        public Resource GetResource(ResourceType type)
+        {
+            return resources.Find(resource => resource.ResourceType == type);
+        }
+
+        public int ModifyResourceAmount(ResourceType type, int change)
+        {
+            var resource = GetResource(type);
+            if (resource.Amount + change < 0)
+            {
+                return resource.Amount;
+            }
+
+            if (!resources.Remove(resource))
+            {
+                return resource.Amount;
+            }
+            
+            var newResource = new Resource(type, resource.Icon, resource.Amount + change);
+            resources.Add(newResource);
+            return newResource.Amount;
+        }
+
+        public bool HasEnoughResource(ResourceType type, int desiredAmount)
+        {
+            return GetResource(type).Amount >= desiredAmount;
         }
     }
 }
